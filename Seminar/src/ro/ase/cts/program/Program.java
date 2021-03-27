605c617b9fc786afa10ca7828370259dda3ef5fc
WINDOWS <pirvuana18@stud.ase.ro>
2021-03-27 14:57:44 +0200

    Adaugare Readere

---
+++ Seminar/src/ro/ase/cts/program/Program.java
@@ -0,0 +1,24 @@
+package ro.ase.cts.program;
+
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.reader.ReaderAngajati;
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Angajat> listaAngajati;
+		try {
+			listaAngajati = ReaderAngajati.readAngajati("angajati.txt");
+			for(Angajat angajat:listaAngajati)
+				System.out.println(angajat.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
