40b5bf687ac660c74ef4a8062e0c6aae0490ccc0
WINDOWS <pirvuana18@stud.ase.ro>
2021-05-08 11:03:36 +0300

    Command + ChainOfResponsability

---
+++ Seminar10/src/ro/ase/cts/chainofresponsability/clase/Cont.java
@@ -0,0 +1,27 @@
+package ro.ase.cts.chainofresponsability.clase;
+
+public abstract class Cont {
+
+	private float sold;
+	private Cont cont;
+		
+	public Cont(float sold) {
+		super();
+		this.sold = sold;
+		this.cont=null;
+	}
+	public float getSold() {
+		return sold;
+	}
+	public Cont getCont() {
+		return cont;
+	}
+	public void setSold(float sold) {
+		this.sold = sold;
+	}
+	public void setCont(Cont cont) {
+		this.cont = cont;
+	}
+	
+	public abstract void realizeazaPlata(float suma);
+}
